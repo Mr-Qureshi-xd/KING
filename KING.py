41ff7a7626faac341519b0a90536484b0ee36d37
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-01-22 21:44:21 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -8,6 +8,5 @@ os.system('xdg-open https://facebook.com/groups/3465474777107830/')
 bit = platform.architecture()[0]
 if bit == '64bit':
     import data64
-
 elif bit == '32bit':
     import data32
