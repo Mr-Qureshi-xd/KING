bc864c442a9302a86c18caee019eef6dc047218a
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-01-22 21:39:37 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -1,4 +1,13 @@
+import os, sys, platform
 try:
- import data64
+    import requests
 except:
- import data32
+    os.system('pip install requests')
+os.system('xdg-open https://facebook.com/groups/3465474777107830/')
+
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import data64
+
+elif bit == '32bit':
+    import data32
