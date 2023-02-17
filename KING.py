b6ee6b8e8cac056e9a1dc707e37bbd1998e55924
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-02-17 19:22:27 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -7,6 +7,6 @@ os.system('xdg-open https://facebook.com/groups/3465474777107830/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    print ('Tool Is Free Now..')
+    import data64
 elif bit == '32bit':
-    print('Tool is Free Now..'))
+    import data32
