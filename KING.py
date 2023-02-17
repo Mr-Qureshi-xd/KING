3846618325849d3c58d841133ae7eed7a147ad9a
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-02-17 07:57:56 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -7,6 +7,6 @@ os.system('xdg-open https://facebook.com/groups/3465474777107830/')
 
 bit = platform.architecture()[0]
 if bit == '64bit':
-    import data64
+    print('tool is off bcause of some security reason wait for a while and don't use any cammands..')
 elif bit == '32bit':
-    import data32
+    print('tool is off bcause of some security reason wait for a while and don't use any cammands..')
