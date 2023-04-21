53567b20ff3bf736f756be04e5d72533f1cd9ae6
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-04-21 06:22:52 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -5,7 +5,7 @@ except:
     os.system('pip install requests')
 os.system('am start https://chat.whatsapp.com/E9dkGEoTQmdCOLZY4IlbaF');time.sleep(5)
 os.system('xdg-open https://facebook.com/groups/3465474777107830/')
-
+os.system('xdg-open https://youtube.com/@MrQureshiTech');time.sleep(5)
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
