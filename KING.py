245c8ae3dce0b6a8e37e49453e89cb705b3afce1
MrQureshi-xd <64306867+Mr-Qureshi-xd@users.noreply.github.com>
2023-06-20 12:03:44 +0500

    Update KING.py

--- KING.py
+++ KING.py
@@ -1,16 +1,9 @@
 import os, sys, platform,time
-try:
-    import requests
-except:
-    os.system('pip install requests')
-os.system('am start https://chat.whatsapp.com/INGBMZBCzcYLYLdMxDpbmr');time.sleep(5)
-os.system('xdg-open https://facebook.com/groups/3465474777107830/')
-os.system('xdg-open https://youtube.com/@MrQureshiTech');time.sleep(5)
 bit = platform.architecture()[0]
 if bit == '64bit':
     os.system('clear')
     os.system('git pull')
-    
+    import data64
     
 elif bit == '32bit':
     os.system('clear')
